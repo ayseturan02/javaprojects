d5f99b6c510d9d56955fc3b0a0f249155bfaa66b
AYŞE TURAN <ayse0025turan@gmail.com>
2023-10-27 17:50:36 +0300

    Create basic.java
    simple java shortcodes

---
+++ basic.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaproject;
+
+/**
+ *
+ * @author ayse0
+ */
+public class Javaproject {
+
+    public static void main(String[] args) {
+        System.out.println("merhaba dünya");
+        //unutmamak için not tutuyorum.
+        System.out.println("ben ayse turan");
+        
+        int x = 10;
+        int y;
+        int ayse;
+        ayse = 10;
+        System.out.println("merhaba dünya "+ ayse );
+        ayse = 20;
+        System.out.println("merhaba dünya " + ayse );
+        
+        double pi=3.14;
+        String s="benim ismim ayse";
+        System.out.println("evet "+s +"pi: " +pi);
+        System.out.println("pi :"+pi);
+              
+    }
+}
